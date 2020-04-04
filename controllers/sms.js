bf5421f8ae6a33b2946f9552f789ca5a31ed498f
Chaitanyakrishna <chaitanyadukkipaty@gmail.com>
2020-04-04 22:35:29 +0530

    smsApi validations added

--- controllers/sms.js
+++ controllers/sms.js
@@ -1,20 +1,25 @@
-module.exports = (
-  {
-    smsService
-  }
-) => {
+module.exports = ({
+  smsService,
+  loginOtpApiValidation,
+  sendImmunopassPassApiValidation,
+  sendImmunopassVoucherApiValidation
+}) => {
   async function loginOtpApi({ to, otp }) {
+    await loginOtpApiValidation({ to, otp });
     const sms = {
       to: to,
       body: `Your login OTP is ${otp}}`,
       priority: "High"
     };
+    const d = await smsService.send(sms);
+    // @todo remove the log later
+    console.log(d);
 
-    await smsService.send(sms);
     return true;
   }
 
   async function sendImmunopassPassApi({ to, token }) {
+    await sendImmunopassPassApiValidation({ to, token });
     const sms = {
       to: to,
       body: `You have been assigned a ImmunoPass for a coronavirus. Your ImmunePass token is ${token}}. Call 6789 for further information.`,
@@ -26,6 +31,7 @@ module.exports = (
   }
 
   async function sendImmunopassVoucherApi({ to, voucherCode }) {
+    await sendImmunopassVoucherApiValidation({ to, voucherCode });
     const sms = {
       to: to,
       body: `You have been assigned a voucher for a coronavirus immunity test. Your voucher code is ${voucherCode}. Please wait for further instructions from your employer. Call 6789 for further information.`,
--- dependencies/smsDependencies.js
+++ dependencies/smsDependencies.js
@@ -1,5 +1,13 @@
-const smsService = require('../service/sms');
+const smsService = require("../service/sms");
+const {
+  loginOtpApiValidation,
+  sendImmunopassPassApiValidation,
+  sendImmunopassVoucherApiValidation
+} = require("../infra/validations/smsApiValidations");
 
 module.exports = {
-  smsService
-}
+  smsService,
+  loginOtpApiValidation,
+  sendImmunopassPassApiValidation,
+  sendImmunopassVoucherApiValidation
+};
